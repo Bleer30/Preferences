29a9a79a2463f5c009dcfc468de45928d342b26a
Bleer30 <stif2001@gmail.com>
2023-01-12 16:42:38 -0600

    add project

---
+++ Preference_json/Preferences.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preference_json
+{
+    
+    internal class Preferences
+    {
+        public int DNI;
+        public string Input;
+        public string State;
+        public Radio BtnRadio;
+
+        public Preferences(int dni, string input, string state, Radio radio)
+        {
+            DNI = dni;
+            Input = input;
+            State = state;
+            BtnRadio = radio;
+        }
+    }
+
+    public class Radio
+    {
+        public enum Tipo
+        {
+            None = 0,
+            A = 1,
+            B = 2
+        }
+
+        public Tipo tipo;
+        public string input;
+    }
+
+    public class DataBase<T>
+    {
+        public List<T> values = new List<T>();
+        public string ruta;
+
+        public DataBase(string r)
+        {
+            ruta = r;
+        }
+
+        public void Save()
+        {
+            string texto = JsonConvert.SerializeObject(values);
+            File.WriteAllText(ruta, texto);
+        }
+
+        public void Load()
+        {
+            string archivo = File.ReadAllText(ruta);
+            values = JsonConvert.DeserializeObject<List<T>>(archivo);
+        }
+
+        public void Insertar(T nuevo)
+        {
+            values.Add(nuevo);
+            Save();
+        }
+
+        public List<T> Buscar(Func<T, bool> criterio)
+        {
+            return values.Where(criterio).ToList();
+        }
+
+        public void Eliminar (Func<T, bool> criterio)
+        {
+            values = values.Where(x => !criterio(x)).ToList();
+        }
+
+        public void Actualizar(Func<T, bool> criterio, T nuevo)
+        {
+            values = values.Select(X =>
+            {
+                if (criterio(X)) X = nuevo;
+                return X;
+            }).ToList();
+        }
+    }
+}
---
+++ Preference_json/frnMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Preference_json;
+using static System.Windows.Forms.AxHost;
+
+namespace Preference_json
+{
+    public partial class frnMain : Form
+    {
+        DataBase<Preferences> bd = new DataBase<Preferences>("bd.json");
+        
+        void mostrar(List<Preferences> list)
+        {
+            foreach (Preferences p in list)
+            {
+                this.textBox1.Text = p.Input.ToString();
+                this.WindowState = (FormWindowState)Enum.Parse(typeof(FormWindowState), p.State.ToString());
+                if(p.BtnRadio.tipo == 0)
+                {
+                    radioButton1.Checked = true;
+                }
+                if(p.BtnRadio.tipo > 0)
+                {
+                    radioButton2.Checked = true;
+                }
+            }
+        }
+
+        public frnMain()
+        {
+            InitializeComponent();
+            bd.Load();
+            mostrar(bd.values);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var s = this.WindowState;
+            Radio r = new Radio();
+            Preferences p = new Preferences((new Random()).Next(1000, 9999), textBox1.Text, s.ToString(), r);
+            bd.Insertar(p);
+            mostrar(bd.values);
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    int DNI = int.Parse(textBox1.Text);
+        //    var s = this.WindowState;
+        //    Radio r = new Radio();
+        //}
+    }
+}
